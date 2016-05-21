c6cc95488a4fb746f0c8a8e494a6d6d1b1aa117f
yangyang861115 <yangyang861115@gmail.com>
2016-05-21 00:23:06 -0700

    fixed navbar style and footer style

--- js/app.js
+++ js/app.js
@@ -26,6 +26,11 @@
             .when("/resources", {
                 templateUrl: "partials/resources.html"
             })
+            .when("/message/:type", {
+                templateUrl: "partials/message.html",
+                controller: "MessageController",
+                controllerAs: "model"
+             })
             .when("/support", {
                 templateUrl: "partials/support.html"
             })
@@ -51,22 +56,87 @@
      */
 
     app.controller('PageCtrl', pageCtrl);
+    app.controller('MessageController', messageController);
 
-    function pageCtrl($scope, $location, $http, $sce) {
+    function pageCtrl($scope, $location,$window, $anchorScroll, $http, $sce) {
 
         $scope.scrollPos = 0;
+        //scroll to section_id
+        //$scope.scrollTo = function(id) {
+        //    var old = $location.hash();
+        //    $location.hash(id);
+        //    $anchorScroll();
+        //    //reset to old to keep any additional routing logic from kicking in
+        //    $location.hash(old);
+        //};
+
+        $scope.scrollTo = function(eID) {
+                // This scrolling function
+                // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+
+                var startY = currentYPosition();
+                var stopY = elmYPosition(eID);
+                var distance = stopY > startY ? stopY - startY : startY - stopY;
+                if (distance < 100) {
+                    scrollTo(0, stopY); return;
+                }
+                var speed = Math.round(distance / 100);
+                if (speed >= 20) speed = 20;
+                var step = Math.round(distance / 25);
+                var leapY = stopY > startY ? startY + step : startY - step;
+                var timer = 0;
+                if (stopY > startY) {
+                    for ( var i=startY; i<stopY; i+=step ) {
+                        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                        leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+                    } return;
+                }
+                for ( var i=startY; i>stopY; i-=step ) {
+                    setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                    leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+                }
+
+                function currentYPosition() {
+                    // Firefox, Chrome, Opera, Safari
+                    if (self.pageYOffset) return self.pageYOffset;
+                    // Internet Explorer 6 - standards mode
+                    if (document.documentElement && document.documentElement.scrollTop)
+                        return document.documentElement.scrollTop;
+                    // Internet Explorer 6, 7 and 8
+                    if (document.body.scrollTop) return document.body.scrollTop;
+                    return 0;
+                }
+
+                function elmYPosition(eID) {
+                    var elm = document.getElementById(eID);
+                    var y = elm.offsetTop;
+                    var node = elm;
+                    while (node.offsetParent && node.offsetParent != document.body) {
+                        node = node.offsetParent;
+                        y += node.offsetTop;
+                    } return y;
+                }
+
+        };
+
 
         //render html code
         $scope.renderHtml = function (html_code) {
             return $sce.trustAsHtml(html_code);
-        }
+        };
 
         //show or hide the jumbotron
         $scope.checkURl = function () {
             if ($location.url() == '/' || $location.url().indexOf('/#') > -1) return true;
             return false;
-        }
+        };
 
+        //footer class
+        $scope.checkURlForFooter = function(){
+            console.log($window);
+            if($location.url().indexOf('message') > -1 && $window.innerWidth > 450) return true;
+            return false;
+        }
 
         $scope.$on('$locationChangeStart', function(event) {
             $scope.checkbg = ! $scope.checkURl();
@@ -103,6 +173,12 @@
 
 
 
+    }
+
+    function messageController($routeParams) {
+        var vm = this;
+        vm.type = $routeParams.type;
+        console.log(vm.type)
     }
 
 })();
